79032c423babd29091082cf5099427b78229e103
Joanna Musialkowska <jzmusialkowska@gmail.com>
2022-08-28 11:27:02 +0200

    inheritance commit

---
+++ C# excercises/inheritance/basics.cs
@@ -0,0 +1,21 @@
+﻿namespace Basic;
+
+public class A
+{
+    public void Method1()
+    {
+        // Method implementation.
+    }
+}
+
+public class B : A
+{ }
+
+public class Example
+{
+    public static void Main()
+    {
+        B b = new();
+        b.Method1();
+    }
+}
---
+++ C# excercises/inheritance/simple-class2.cs
@@ -0,0 +1,11 @@
+﻿public class EmptyClass
+{ }
+
+public class ClassNameExample
+{
+    public static void Main()
+    {
+        EmptyClass sc = new();
+        Console.WriteLine(sc.ToString());
+    }
+}
