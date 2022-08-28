3233c86a2cfd4a30aa06efb845e6ec74be4450e3
Joanna Musialkowska <jzmusialkowska@gmail.com>
2022-08-28 12:28:00 +0200

    attributes initial commit

---
+++ C# excercises/attributes/Program.cs
@@ -0,0 +1,23 @@
+﻿public class MyUIClass : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private string _name;
+    public string Name
+    {
+        get { return _name;}
+        set
+        {
+            if (value != _name)
+            {
+                _name = value;
+                RaisePropertyChanged();   
+            }
+        }
+    }
+}
